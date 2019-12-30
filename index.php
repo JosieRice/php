48fc53d51262fec9c8d01c8f4c99902718aac4aa
Craig Rice <craigricetong@gmail.com>
2019-12-30 10:24:47 -0800

    referencing php output for forms

--- index.php
+++ index.php
@@ -1,21 +1,32 @@
 <html>
- <head>
-  <title>PHP Test</title>
- </head>
- <body>
-  <?php echo '<p>Hello World!</p>'; ?>
-  <?php
-    if (strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE') !== FALSE) {
-  ?>
-    <h3>strpos() must have returned non-false</h3>
-    <p>You are using Internet Explorer</p>
-  <?php
-    } else {
-  ?>
-    <h3>strpos() must have returned false</h3>
-    <p>You are not using Internet Explorer</p>
-  <?php
-    }
-  ?>
- </body>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+    <!-- Basic PHP html tag -->
+    <?php echo '<p>Hello World!</p>'; ?>
+
+    <!-- Checking for browser type -->
+    <?php
+      if (strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE') !== FALSE) {
+    ?>
+      <h3>strpos() must have returned non-false</h3>
+      <p>You are using Internet Explorer</p>
+    <?php
+      } else {
+    ?>
+      <h3>strpos() must have returned false</h3>
+      <p>You are not using Internet Explorer</p>
+    <?php
+      }
+    ?>
+
+    <!-- Forms with PHP -->
+    <form action="action.php" method="post">
+      <p>Your name: <input type="text" name="name" /></p>
+      <p>Your age: <input type="text" name="age" /></p>
+      <p><input type="submit" /></p>
+    </form>
+
+  </body>
 </html>
